9e7ed7407e56a4b14f59ff154e4484dc83043e96
SluvsP <salomekbg@gmail.com>
2016-12-01 17:57:24 -0500

    update

--- src/actions/getUser.js
+++ src/actions/getUser.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 export default function getUser(formData) {
   return function(dispatch){
     $.ajax({url: "http://localhost:3000/user",
-    type: "GET",
+    type: "POST",
     headers: {authorization: localStorage.getItem("authorization")}}).done(function(data){
       debugger
       dispatch({type: 'GET_USER', payload: data})
