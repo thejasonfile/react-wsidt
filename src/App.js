eda28b3217bfab6dcc4a4e547aac8e85d850cb47
SluvsP <salomekbg@gmail.com>
2016-12-02 17:14:43 -0500

    add some test style

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default class App extends Component {
   render() {
     return(
       <div>
+        <h1>What Should I Do Tonight?</h1>
         <ul>
-          What Should I do tonight?
           <li><Link to={'/signin'}>Sign In</Link></li>
           <li><Link to={'/newuser'}>New User</Link></li>
         </ul>
