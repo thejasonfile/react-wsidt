8172a635256a7ba9293c7667ce87d35e34a365d8
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-06 14:17:41 -0500

    add key to restaurants array

--- src/actions/createUser.js
+++ src/actions/createUser.js
@@ -15,11 +15,5 @@ export default function createUser(formValues){
       dispatch({type: 'LOGIN_USER', current_user: response.current_user})
       browserHistory.push('/menu')
     })
-    // 1. make an ajax request to my rails
-    // 2. rails will create the user, and in exchange give me back a
-    // jwt, user_id
-    // 3. store the jwt in local storage
-    // 4. update the store that we are logged in, current_user id
-    // 5. redirect to my task resource
   }
 }
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router';
 
 class Restaurants extends Component {
   render(){
-    var restaurants = this.props.restaurants.map((restaurant) => {
+    var restaurants = this.props.restaurants.map((restaurant, index) => {
       return (
-          <ul>
+          <ul key={index}>
             <li><h2>{restaurant.name}</h2></li>
             <li>Rating: {restaurant.rating}</li>
             <li>Address: {restaurant.address}, {restaurant.city} {restaurant.zipcode}</li>
             <li>Phone: {restaurant.phone}</li>
             <li className="last"><a href={restaurant.url} target="_blank">URL: Find on Yelp</a></li>
-            <label for="fav_restaurant">Mark as Favorite</label>
+            <label htmlFor="fav_restaurant">Mark as Favorite</label>
             <input type="checkbox" name="fav_restaurant" id="fav_restaurant" />
             <br />
           </ul>
