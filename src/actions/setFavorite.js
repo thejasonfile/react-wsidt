c489fbdf59db0d558ed90cbb53cdfa629ba1b3b9
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-07 15:44:43 -0500

    create favorite on checkbox checkk

---
+++ src/actions/setFavorite.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+
+export default function setFavorite(restaurant_id, checked){
+  var url = ''
+  var type = ''
+  if (checked) {
+    url = 'http://localhost:3000/favorites'
+    type = 'POST'
+  } else {
+    url = `http://localhost:3000/favorites/${restaurant_id}`
+    type = 'DELETE'
+  }
+
+  return function (dispatch){
+    $.ajax({
+      url: url,
+      type: type,
+      data: JSON.stringify({restaurant: {id: restaurant_id}, user: {jwt: localStorage.getItem('jwt')}}),
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8'
+    })
+  }
+}
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
+import { bindActionCreators } from 'redux'
+import setFavorite from '../actions/setFavorite'
 
 class Restaurants extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      favorites: []
+    }
+  }
   render(){
     var restaurants = this.props.restaurants.map((restaurant, index) => {
       return (
@@ -13,7 +22,7 @@ class Restaurants extends Component {
             <li>Phone: {restaurant.phone}</li>
             <li className="last"><a href={restaurant.url} target="_blank">URL: Find on Yelp</a></li>
             <label htmlFor="fav_restaurant">Mark as Favorite</label>
-            <input type="checkbox" name="fav_restaurant" id="fav_restaurant" />
+            <input type="checkbox" onChange={this.handleFavoriteCheckBox.bind(this)} name={restaurant.name} id={restaurant.id} />
             <br />
           </ul>
       )
@@ -28,10 +37,19 @@ class Restaurants extends Component {
       </div>
     )
   }
+
+  handleFavoriteCheckBox(event){
+    console.log(event.target.checked)
+    this.props.setFavorite(event.target.id, event.target.checked)
+  }
 }
 
 function mapStateToProps(state){
   return {restaurants: state.restaurantsReducer.restaurants, user_id: state.restaurantsReducer.user_id}
 }
 
-export default connect(mapStateToProps)(Restaurants);
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ setFavorite }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Restaurants);
