0a1927af2ca44481e3ccf0501537ce30e79450b6
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-11 14:39:11 -0500

    events displaying on page

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,9 +3,19 @@ import { connect } from 'react-redux';
 
 class Events extends Component {
   render(){
+    debugger
+      var liveEvents = this.props.liveEvents.event_info.map((liveEvent, index) => {
+        return(
+          <ul key={index}>
+            <li><h2><a href={liveEvent.url} target="_blank">{liveEvent.title}</a></h2></li>
+            <li>{liveEvent.description}</li>
+            <li>Start Time: {liveEvent.start_time}</li>
+          </ul>
+        )
+      })
     return(
-      <div>
-        Events go here!
+      <div className="main">
+        {liveEvents}
       </div>
     )
   }
@@ -13,7 +23,7 @@ class Events extends Component {
 
 function mapStateToProps(state){
   return {
-    movies: state.eventsReducer.events
+    liveEvents: state.eventsReducer.events
   }
 }
 
