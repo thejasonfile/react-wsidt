fa70de3b4c21fd0b57407faaeea240f219f74695
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-11 13:13:36 -0500

    change getLiveEvent action creator

--- src/actions/getLiveEvent.js
+++ src/actions/getLiveEvent.js
@@ -1,17 +1,17 @@
 import $ from 'jquery';
 import { browserHistory } from 'react-router';
 
-export default function getRestaurant(formValues){
+export default function getEvent(formValues){
   return function (dispatch){
     $.ajax({
-      url: 'http://localhost:3000/restaurants',
+      url: 'http://localhost:3000/events',
       type: 'POST',
       headers: {authorization: localStorage.getItem('jwt')},
       data: JSON.stringify({data: {zipcode: formValues.zipcode}}),
       dataType: 'json',
       contentType: 'application/json; charset=utf-8'
     }).done(function(response){
-      dispatch({type: 'SHOW_RESTAURANTS', restaurant_info: response})
+      dispatch({type: 'SHOW_EVENTS', event_info: response})
       browserHistory.push('/events')
     })
   }
