59719802938a130929c9159de828d926b3cdc3ed
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-14 18:07:28 -0500

    add bootstrap

--- src/components/menu.js
+++ src/components/menu.js
@@ -12,17 +12,16 @@ import getTV from '../actions/getTV'
 class Index extends Component {
   render(){
     return(
-      <div className="account_info">
+      <div className="account_info form-group">
         <label> Enter Zip Code: </label>
         <input type='number' onChange={this.handleOnZipCodeChange.bind(this)} value={this.props.zipcode} />
         <br/><br/>
-        <button onClick={this.handleRestaurantButtonClick.bind(this)}>Get Restaurants</button>
-        <button onClick={this.handleBarButtonClick.bind(this)}>Get Bars</button>
-        <button onClick={this.handleMovieButtonClick.bind(this)}>Get Movies</button>
-        <button onClick={this.handleLiveEventsButtonClick.bind(this)}>Get Live Events</button>
-        <button onClick={this.handleConcertButtonClick.bind(this)}>Get Concerts</button>
-        <button onClick={this.handleTVButtonClick.bind(this)}>On TV?</button>
-        <button>Choose For Me!</button>
+        <button className='btn btn-lg' onClick={this.handleRestaurantButtonClick.bind(this)}>Get Restaurants</button>
+        <button className='btn btn-lg' onClick={this.handleBarButtonClick.bind(this)}>Get Bars</button>
+        <button className='btn btn-lg' onClick={this.handleMovieButtonClick.bind(this)}>Get Movies</button>
+        <button className='btn btn-lg' onClick={this.handleLiveEventsButtonClick.bind(this)}>Get Live Events</button>
+        <button className='btn btn-lg' onClick={this.handleConcertButtonClick.bind(this)}>Get Concerts</button>
+        <button className='btn btn-lg' onClick={this.handleTVButtonClick.bind(this)}>On TV?</button>
       </div>
     )
   }
