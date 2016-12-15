d2f4cfa5cd8f0b426fcb817f2e67efcea9b1266b
SluvsP <salomekbg@gmail.com>
2016-12-15 14:46:25 -0500

    style streaming and movie component

--- src/components/TVShows.js
+++ src/components/TVShows.js
@@ -5,13 +5,11 @@ class Shows extends Component {
   render(){
     var streamingMovies = this.props.shows.movies_results.map((movie, index) => {
       return (
-          <div key={index}>
-            <ul key={index} className="tvlist">
-              <li><img className="tv" src={movie.image} alt='movie' /></li>
-              <li><h2><a href={movie.url} target="_blank">{movie.title}</a></h2></li>
-              <li>Rated: {movie.rating}</li><br />
-              <li className="last"><u>Release Date:</u> {movie.release_date}</li>
-            </ul>
+          <div key={index} className="col-md-4">
+            <li><img className="movie" src={movie.image} alt='movie' /></li>
+            <li><h2><a href={movie.url} target="_blank">{movie.title}</a></h2></li>
+            <li>Rated: {movie.rating}</li><br />
+            <li className="last"><u>Release Date:</u> {movie.release_date}</li>
           </div>
 
       )
@@ -19,22 +17,24 @@ class Shows extends Component {
 
     var streamingShows = this.props.shows.shows_results.map((show, index) => {
       return (
-          <div key={index}>
-            <ul key={index} className="tvlist2">
-              <li><img className="tv" src={show.image} alt='show' /></li>
-              <li><h2><a href={show.url} target="_blank">{show.title}</a></h2></li>
-              <li className="last"><u>First Aired:</u> {show.first_aired}</li>
-            </ul>
+          <div key={index} className="col-md-4">
+            <li><img className="tv" src={show.image} alt='show' /></li>
+            <li><h2><a href={show.url} target="_blank">{show.title}</a></h2></li>
+            <li className="last"><u>First Aired:</u> {show.first_aired}</li>
           </div>
       )
     })
 
     return(
-      <div className="main">
+      <div className="container main components">
+        <div className='row'>
         <h2 className="shift">Movies:</h2>
-        {streamingMovies}
-        <h2 className="shift">Shows:</h2>
-        {streamingShows}
+          {streamingMovies}
+        </div>
+        <div className='row'>
+        <h2 className="shift">TV Shows:</h2>
+          {streamingShows}
+        </div>
       </div>
     )
   }
