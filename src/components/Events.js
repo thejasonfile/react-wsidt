40668c31458a45c780216b5181ae7e35abb31c16
Jason Arnold <jason.arnold.00@gmail.com>
2016-12-15 14:54:16 -0500

    style events

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 class Events extends Component {
   render(){
       if(this.props.liveEvents.length === 0){
-        var liveEvents = function(){
+        var components = function(){
           return(
             <ul>
               <li>No events in your area today!  Check back later.</li>
@@ -12,20 +12,28 @@ class Events extends Component {
           )
         }
       } else {
-      liveEvents = this.props.liveEvents.map((liveEvent, index) => {
+      components = this.props.liveEvents.map((liveEvent, index) => {
         return(
-          <ul key={index} className="list">
-            <li><img className="event" src={liveEvent.image_url} /></li>
-            <li><h2><a href={liveEvent.url} target="_blank">{liveEvent.title}</a></h2></li>
-            {liveEvent.description !== null ? <li className="description">{liveEvent.description}</li> : <li>No Description Available<br /></li>}<br />
-            <li><u>Start Time:</u> {liveEvent.start_time}</li>
-          </ul>
+          <div key={index} className="col-md-12">
+            <div className="row">
+              <div className="col-md-3">
+                <img src={liveEvent.image_url} />
+                <div className="col-md-9">
+                  <h2><a href={liveEvent.url} target="_blank">{liveEvent.title}</a></h2>
+                  {liveEvent.description !== null ? <li className="description">{liveEvent.description}</li> : <li>No Description Available<br /></li>}<br />
+                  <li><u>Start Time:</u> {liveEvent.start_time}</li>
+                </div>
+              </div>
+            </div>
+          </div>
         )
       })}
 
     return(
-      <div className="main">
-        {liveEvents}
+      <div className="container main components">
+        <div className="row">
+          {components}
+        </div>
       </div>
     )
   }
